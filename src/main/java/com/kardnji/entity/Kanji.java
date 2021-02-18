da165809cb5d7acfe00781465ac7a46bfe6006a8
Bembo <playbimbo@gmail.com>
2021-02-18 13:07:58 -0800

    bprrpt

--- src/main/java/com/kardnji/entity/Kanji.java
+++
@@ -1,9 +0,0 @@
-package com.kardnji.entity;
-
-public class Kanji extends Kard{
-
-	public Kanji(String kanjiImage, int kanjiId, String keyWord, String shortStory) {
-		super(kanjiImage, kanjiId, keyWord, shortStory);
-	}
-	
-}
